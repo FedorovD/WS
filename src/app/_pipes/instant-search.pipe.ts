f0d1565d96c2440fb7b0e517bec6c2cacd12fac0
Danil Fedorov <you@example.com>
2017-05-14 18:08:20 +0300

    merge two types collection onto into one

--- src/app/_pipes/instant-search.pipe.ts
+++ src/app/_pipes/instant-search.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class InstantSearchPipe implements PipeTransform {
 
-    transform(value: any, search: String = ''): any {
+    transform(value: any=[], search: String = ''): any {
     let searchQuery = search;
     let displayedWords = value.filter(el => {
     let searchValueEng = el.english;
